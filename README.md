f612d064a1320add1b7b5e48944212473ae7ec37
kKapalka <k.kapalka@hotmail.com>
2016-12-07 20:48:15 +0100

    dodatkowe operatory

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # zad3-macierze-cpp
 Zadanie 3 - klasa opisująca macierze rzeczywiste. Język - C++
+
+Praca w toku.
+
+V0.2:
+
+Operatory *= (dla liczb, macierzy)
+
+Operatory ==, !=(macierz, macierz) (nie wiem czy potrzeba więcej)
+
+Operator + (macierz, macierz) (uwaga: wyrzuca fajny error jak spróbujesz działania: A=A+B, gdzie A,B to macierze)
+
+
+V0.1:
+
+konstruktory, destruktory
+
+Operatory * (dla liczb i macierzy), <<
+
+Mniej wiecej przejrzyste wyswietlanie
--- macierz.cpp
+++ macierz.cpp
@@ -14,8 +14,8 @@ double **mat;
 
 //konstruktor domyślny i wymiarowy
 Matrix(int _width=2, int _height=2):width(_width), height(_height){
-mat = new double*[width];
-for(int i=0;i<height;i++) mat[i]=new double[height];
+mat = new double*[height];
+for(int i=0;i<height;i++) mat[i]=new double[width];
 }
 
 //konstruktor kopiujący
@@ -33,8 +33,30 @@ for(int i=0;i<height;i++){
 
 //destrkutor
 ~Matrix(){
-for(int i=0;i<width;i++) delete []mat[i];
+for(int i=0;i<height;i++) delete []mat[i];
 delete []mat;
+}
+
+//przeładowany operator *= dla macierzy i liczby
+Matrix operator*=(double a){
+for(int i=0;i<this->height;i++){
+	for(int j=0;j<this->width;j++) this->mat[i][j]*=a;
+	}
+return *this;
+}
+
+//przeladowany operator *= dla dwoch macierzy
+Matrix operator*=( Matrix B){
+if(this->width==B.height){
+	Matrix W(this->height, B.width);
+	for(int i=0;i<W.height;i++){
+		for(int j=0;j<W.width;j++){
+			for(int k=0;k<this->height;k++) W.mat[i][j]+=this->mat[i][k]*B.mat[k][j];
+			}
+		}
+		return W;
+	}
+	else cout<<"Operacja zakończona niepowodzeniem"<<endl;
 }
 
 };
@@ -59,7 +81,9 @@ if(A.width==B.height){
 return W;
 }
 else cout<<"Operacja zakończona niepowodzeniem"<<endl;
+return A;
 };
+
 
 //przeładowany operator <<
 std::ostream & operator<<(std::ostream &s, Matrix& A){
@@ -76,13 +100,45 @@ for(int i=0;i<A.height;i++){
 return s;
 }
 
+//przeładowany operator ==
+bool operator==(Matrix A, Matrix B){
+	if((A.width==B.width)&&(A.height==B.height)){
+		for (int i=0;i<A.height;i++){
+			for(int j=0;j<A.width;j++) if (A.mat[i][j]!=B.mat[i][j]) return false;
+		}
+	return true;
+	}
+	return false;
+	 
+}
+
+//przeladowany operator !=
+bool operator!=(Matrix A, Matrix B){
+	return !(A==B);
+}
+
+//przeladowany operator + (Matrix, Matrix)
+Matrix operator+(Matrix A, Matrix B){
+if((A.width==B.width)&&(A.height==B.height)){
+		for (int i=0;i<A.height;i++){
+			for(int j=0;j<A.width;j++) A.mat[i][j]+=B.mat[i][j];
+		}
+	}
+else cout<<"Operacja niemozliwa. Powod - niezgodnosc wymiarow"<<endl;
+return A;
+}
 
 int main(int argc, char *argv[]){
 Matrix A(6,6);
-A.mat[2][3]=7;
-Matrix B(A);
-Matrix C = B*3.14;
-cout<<C;
 
+A.mat[2][3]=7;
+cout<<A<<endl;
+Matrix B(A);
+cout<<(A!=B)<<endl;
+Matrix C = B*3.14;
+C*=2;
+cout<<C<<endl;
+Matrix D=C+B;
+cout<<D<<endl;
 return 0;
 }
