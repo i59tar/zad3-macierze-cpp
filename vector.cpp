81e8aed5f819c68ff0372c9c99e769f9d94c8d80
kKapalka <k.kapalka@hotmail.com>
2016-12-11 13:55:14 +0100

    Naprawa drobnych usterek

--- vector.cpp
+++ vector.cpp
@@ -15,7 +15,8 @@ Vector(int _len=1):len(_len){vect = new double[len];}
 Vector(double *tab, int _len=2):len(_len){
 vect = new double[len];
 for(int i=0;i<len;i++){
-	while (i<sizeof(tab)/sizeof(*tab))vect[i]=tab[i];
+	if (tab[i])vect[i]=tab[i];
+	else vect[i]=0;
 	}
 }
 //konstruktor kopiujacy
@@ -32,12 +33,10 @@ for(int i=0;i<len;i++) vect[i]=A.vect[i];
 //operator przypisania
 Vector& operator=(const Vector& B){
 if (this!=&B){
-	if(len!=B.len){
-		delete[] vect;
-		len=B.len;
-		vect = new double[len];
-		for(int i=0;i<len;i++) vect[i]=B.vect[i];
-		}
+	delete[] vect;
+	len=B.len;
+	vect = new double[len];
+	for(int i=0;i<len;i++) vect[i]=B.vect[i];
 	}
 return *this;
 }
@@ -53,6 +52,7 @@ Vector& set_size(int a){
 		double* n_vec=new double[a];
 		for(int i=0;i<a;i++)n_vec[i]=i<len?vect[i]:0;
 		len=a;
+		delete[] vect;
 		vect = n_vec;
 		return *this;
 }	
@@ -60,10 +60,14 @@ Vector& set_size(int a){
 Vector& dodaj(double a){
 	double* n_vec = new double(len+1);
 	for (int i=0;i<len+1;i++) n_vec[i]=i<len?vect[i]:a;
+	delete[] vect;
 	vect = n_vec;
 	return *this;
 }
-//***********************************************************//
+//**********************FUNKCJE ZAPRZYJAZNIONE*******************//
+friend std::istream& operator>>(std::istream& is, Vector& A);
+friend std::ostream& operator<<(std::ostream& os, Vector& A);
+//****************************************************//
 private:
 double *vect;
 int len;
@@ -73,13 +77,20 @@ int len;
 
 
 int main(){
-Vector A;
+double tab[4]={2,3.3,5,9};
+Vector A(tab,2);
 A.set_size(3);
-A[0]=5;
-A[1]=9.9;
+A[2]=9.9;
 Vector B=A;
 
 B.set_size(5);
-cout<<B[0]<<' '<<A[1]<<' '<<B[1]<<' '<<B[4]<<endl;
+cout<<B[0]<<' '<<A[1]<<' '<<B[1]<<' '<<B[2]<<endl;
 return 0;
 }
+
+std::istream& operator>>(std::istream& is, Vector& A){
+
+};
+std::ostream& operator<<(std::ostream& os, Vector& A){
+
+};
