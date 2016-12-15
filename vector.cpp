ca1cb1e58713bcb5e07ee7ab254e8e51dc6fabba
kKapalka <k.kapalka@hotmail.com>
2016-12-15 08:32:18 +0100

    Sort Vector

--- vector.cpp
+++ vector.cpp
@@ -5,8 +5,6 @@ using namespace std;
 
 class Vector{
 public:
-
-
 //***************** KONSTRUKTORY I DESTRUKTORY************************************** //
 //konstruktory: domyslny i jednoargumentowy
 Vector(int _len=1):len(_len){vect = new double[len];}
@@ -15,10 +13,8 @@ Vector(int _len=1):len(_len){vect = new double[len];}
 Vector(double *tab, int _len=2):len(_len){
 vect = new double[len];
 int i=0;
-while(i<len && tab[i]){
-	vect[i]=tab[i++];
-	i++;
-}
+while(i<len && tab[i])vect[i]=tab[i++];
+
 while(i<len) vect[i++]=0;
 }
 
@@ -70,14 +66,73 @@ Vector& dodaj(double a){
 //**********************FUNKCJE ZAPRZYJAZNIONE*******************//
 friend std::istream& operator>>(std::istream& is, Vector& A);
 friend std::ostream& operator<<(std::ostream& os, Vector& A);
+friend bool operator==(Vector A, Vector B);
+friend bool operator!=(Vector A, Vector B);
 //****************************************************//
-private:
+protected:
 double *vect;
 int len;
 };
 
+//*************FUNKCJE ZAPRZYJAZNIONE - DEFINICJE***********************//
+std::istream& operator>>(std::istream& is, Vector& A){
+	int _len;	
+	is>>_len;
+	if(_len>A.len) A.set_size(A.len);
+	for(int i=0;i<_len;i++)is>>A[i];
+	return is;
+}
+std::ostream& operator<<(std::ostream& os, Vector& A){
+	os<<A.len<<':';
+	for(int i=0;i<A.len;i++){
+		os<<' ';
+		cout.width(4);
+		os<<A[i];
+	}
+	return os;
+}
+bool operator==(Vector A, Vector B){
+if(A.len!=B.len) return false;
+else for(int i=0;i<A.len;i++) if(A[i]!=B[i])return false;
+return true;
+}
+bool operator!=(Vector A, Vector B){
+return !(A==B);
+}
+//*********************************************************************//
 
 
+//*********************FUNKCJA 'compare' - NA POTRZEBY QSORT**********//
+int compare(const void * a, const void * b){
+if( *(double*)a - *(double*)b>0) return 1;
+else if ( *(double*)a - *(double*)b<0) return -1;
+else return 0;
+}
+
+//*********************************************************************//
+class SVector:public Vector{
+public:
+//***************************KONSTRUKTORY ****************************//
+void Sort(){
+	qsort((void*)vect,len,sizeof(double),compare);
+}
+SVector(int _len=1):Vector(_len){
+Sort();
+}
+SVector(double *tab, int _len=2):Vector(tab,_len){
+Sort();
+}
+SVector(Vector& A):Vector(A){
+Sort();
+}
+SVector(SVector& A){
+len = A.len;
+vect = new double[len];
+for(int i=0;i<len;i++) vect[i]=A.vect[i];
+Sort();
+}
+//
+};
 
 int main(){
 double tab[4]={2,3.3};
@@ -85,14 +140,9 @@ Vector A(tab,8);
 
 A[4]=9.9;
 Vector B=A;
-
-cout<<B[0]<<' '<<A[1]<<' '<<B[1]<<' '<<B[2]<<' '<<B[3]<<' '<<B[4]<<' '<<B[5]<<' '<<B[6]<<' '<<B[7]<<' '<<endl;
+cin>>B;
+SVector C(B);
+cout<<A<<endl<<B<<endl<<C<<endl;
 return 0;
 }
 
-std::istream& operator>>(std::istream& is, Vector& A){
-
-};
-std::ostream& operator<<(std::ostream& os, Vector& A){
-
-};
